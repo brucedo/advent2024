3ac4bb2161df8090f156e12b67fde46e16ab81aa
Doug Bruce <queazan@gmail.com>
2024-12-02 13:14:10 -0600

    - Removed a bunch of unused crap from previous attempts.  Yeeeeesh.

--- lib/day2.ex
+++ lib/day2.ex
@@ -33,22 +33,6 @@ defmodule Day2 do
     |> Enum.count()
   end
 
-  @spec dampened_safe?(list(integer())) :: boolean()
-  def dampened_safe?(readings) do
-    safety_reading = dampened_monotonic?(readings) && dampened_gradual?(readings)
-
-    if safety_reading do
-      Logger.debug("#{Enum.map(readings, fn num -> Integer.to_string(num) <> "," end)} is safe.")
-      safety_reading
-    else
-      Logger.debug(
-        "#{Enum.map(readings, fn num -> Integer.to_string(num) <> "," end)} IS NOT SAFE"
-      )
-
-      safety_reading
-    end
-  end
-
   @spec all_perms(list(integer())) :: list(list(integer()))
   def all_perms([_]) do
     []
@@ -57,57 +41,6 @@ defmodule Day2 do
   def all_perms(to_sublist) do
     for index <- Range.new(0, length(to_sublist) - 1) do
       List.delete_at(to_sublist, index)
-    end
-  end
-
-  @spec dampened_monotonic?(list(integer())) :: boolean()
-  def dampened_monotonic?(readings) do
-    [left | [right | _]] = readings
-
-    starting_direction = direction(left, right)
-
-    test_monotonic_counting_failures(readings, starting_direction, 0) <= 1
-  end
-
-  def test_monotonic_counting_failures([_ | []], _, fail_count) do
-    fail_count
-  end
-
-  def test_monotonic_counting_failures([left | [right | rest]], direction, fail_count) do
-    dir = direction(left, right)
-
-    if direction == dir do
-      test_monotonic_counting_failures([right | rest], direction, fail_count)
-    else
-      test_monotonic_counting_failures([right | rest], direction, fail_count + 1)
-    end
-  end
-
-  @spec dampened_gradual?(list(integer())) :: boolean()
-  def dampened_gradual?(readings) do
-    test_gradual_counting_failures(readings, 0) <= 1
-  end
-
-  defp test_gradual_counting_failures([_ | []], fail_count) do
-    fail_count
-  end
-
-  defp test_gradual_counting_failures([first | [second | rest]], fail_count) do
-    difference = abs(first - second)
-
-    if difference == 0 || difference > 3 do
-      test_gradual_counting_failures([second | rest], fail_count + 1)
-    else
-      test_gradual_counting_failures([second | rest], fail_count)
-    end
-  end
-
-  @spec direction(integer(), integer()) :: atom()
-  defp direction(left, right) do
-    if left > right do
-      :desc
-    else
-      :asc
     end
   end
 
