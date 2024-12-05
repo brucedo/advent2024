d68c8cdd03ee0d7d34d0cffbd1d205f857dcbba9
Doug Bruce <queazan@gmail.com>
2024-12-04 21:02:22 -0600

    Never liked grids much.  Still don't.

--- lib/day4.ex
+++ lib/day4.ex
@@ -2,11 +2,140 @@ defmodule Point do
   defstruct x: -1, y: -1
 end
 
+defmodule Grid do
+  defstruct width: -1, height: -1, map: %{}
+end
+
 defmodule Day4 do
+  require Logger
+
+  def run() do
+    lines =
+      Common.open(File.cwd(), "day4.txt") |> Common.read_file_pipe() |> Common.close()
+
+    height = length(lines)
+    width = Enum.map(lines, fn line -> String.length(line) end) |> Enum.max()
+
+    Logger.debug("Computed height and width: #{height}x#{width}")
+
+    pre_grid =
+      Enum.with_index(lines)
+      |> Enum.map(fn {text, index} -> {indexer(text), index} end)
+      |> Enum.map(fn {indexed_list, index} -> pointifier(indexed_list, index) end)
+      |> Enum.flat_map(fn point -> point end)
+      |> Enum.reduce(%{}, fn {point, letter}, acc -> Map.put(acc, point, letter) end)
+
+    # Logger.debug("Computed map: #{pre_grid}")
+
+    grid = %Grid{width: width, height: height, map: pre_grid}
+
+    appearances = word_count(grid)
+
+    IO.puts("Total count of appearances of XMAS: #{appearances}")
+
+    cross_appearances = cross_word_count(grid)
+    IO.puts("Total count of appearances of MAS in X: #{cross_appearances}")
+  end
+
+  @spec cross_word_count(%Grid{}) :: integer()
+  def cross_word_count(grid) do
+    up_left = %Point{x: -1, y: -1}
+    down_right = %Point{x: 1, y: 1}
+
+    up_right = %Point{x: 1, y: -1}
+    down_left = %Point{x: -1, y: 1}
+
+    starting_points =
+      Enum.filter(grid.map, fn {_, letter} -> letter == "A" end)
+      |> Enum.map(fn {point, _} -> point end)
+
+    Enum.map(starting_points, fn a_point ->
+      {
+        %Point{x: a_point.x + up_left.x, y: a_point.y + up_left.y},
+        %Point{x: a_point.x + down_right.x, y: a_point.y + down_right.y},
+        %Point{x: a_point.x + up_right.x, y: a_point.y + up_right.y},
+        %Point{x: a_point.x + down_left.x, y: a_point.y + down_left.y}
+      }
+    end)
+    |> Enum.map(fn {up_left, down_right, up_right, down_left} ->
+      {
+        (letter_check(grid, up_left, "S") && letter_check(grid, down_right, "M")) ||
+          (letter_check(grid, up_left, "M") && letter_check(grid, down_right, "S")),
+        (letter_check(grid, up_right, "S") && letter_check(grid, down_left, "M")) ||
+          (letter_check(grid, up_right, "M") && letter_check(grid, down_left, "S"))
+      }
+    end)
+    |> Enum.map(fn {right_diagonal, left_diagonal} -> right_diagonal && left_diagonal end)
+    |> Enum.filter(fn t -> t end)
+    |> Enum.count()
+  end
+
+  @spec word_count(%Grid{}) :: integer()
+  def word_count(grid) do
+    target = ["X", "M", "A", "S"]
+
+    directions = [
+      %Point{x: 1, y: 1},
+      %Point{x: 1, y: 0},
+      %Point{x: 1, y: -1},
+      %Point{x: 0, y: -1},
+      %Point{x: -1, y: -1},
+      %Point{x: -1, y: 0},
+      %Point{x: -1, y: 1},
+      %Point{x: 0, y: 1}
+    ]
+
+    starting_points =
+      Enum.filter(grid.map, fn {_, letter} -> letter == List.first(target) end)
+      |> Enum.map(fn {point, _} -> point end)
+
+    Enum.map(starting_points, fn point ->
+      for direction <- directions do
+        scan(grid, point, direction, target)
+      end
+    end)
+    |> Enum.flat_map(fn t -> t end)
+    |> Enum.filter(fn t -> t end)
+    |> Enum.count()
+  end
+
+  @spec scan(%Grid{}, %Point{}, %Point{}, list(String.t())) :: boolean()
+  def scan(_, _, _, []) do
+    true
+  end
+
+  def scan(grid, cell, direction, [letter | rest]) do
+    case letter_check(grid, cell, letter) do
+      true ->
+        scan(grid, %Point{x: cell.x + direction.x, y: cell.y + direction.y}, direction, rest)
+
+      a ->
+        a
+    end
+  end
+
+  @spec letter_check(%Grid{}, %Point{}, String.t()) :: boolean()
+  def letter_check(grid, point, wanted) do
+    if(point.x >= grid.width || point.y >= grid.height) do
+      false
+    else
+      Map.get(grid.map, point) == wanted
+    end
+  end
+
   @spec indexer(String.t()) :: list({String.t(), integer()})
   def indexer(letter_sequence) do
     String.splitter(letter_sequence, "")
     |> Enum.filter(fn letter -> letter != "" end)
     |> Enum.with_index()
+  end
+
+  @spec pointifier(list({String.t(), integer()}), integer()) :: list({%Point{}, String.t()})
+  def pointifier([], _) do
+    []
+  end
+
+  def pointifier([{letter, column} | character_map], row_number) do
+    [{%Point{x: column, y: row_number}, letter} | pointifier(character_map, row_number)]
   end
 end
--- test/day4_test.exs
+++ test/day4_test.exs
@@ -19,103 +19,220 @@ defmodule Day4Test do
            ]
   end
 
-  test "Given some list of tuples of the form {letter, index} and the current row number, gridifier() will return a Map of Point->letter" do
-    tuple_list = [{"X", 0}, {"M", 1}, {"I", 2}, {"S", 3}, {"S", 4}]
+  test "Given a tuple {[letters], row_index} then pointifier() will construct a [{Point, letter}]" do
+    list = [{"X", 0}, {"M", 1}, {"I", 2}, {"S", 3}, {"S", 4}]
 
-    assert Day4.gridifier(tuple_list, 1) == %{
-             %Point{x: 0, y: 1} => "X",
-             %Point{x: 1, y: 1} => "M",
-             %Point{x: 2, y: 1} => "I",
-             %Point{x: 3, y: 1} => "S",
-             %Point{x: 4, y: 1} => "S"
-           }
+    assert Day4.pointifier(list, 1) == [
+             {%Point{x: 0, y: 1}, "X"},
+             {%Point{x: 1, y: 1}, "M"},
+             {%Point{x: 2, y: 1}, "I"},
+             {%Point{x: 3, y: 1}, "S"},
+             {%Point{x: 4, y: 1}, "S"}
+           ]
+  end
+
+  test "Given a Grid with the letter 'X' at %Point{x:1, y:1} then letter_check(Grid, Point, 'X') will return true " do
+    grid = %Grid{
+      width: 2,
+      height: 2,
+      map: %{%Point{x: 0, y: 0} => "M", %Point{x: 1, y: 1} => "X"}
+    }
+
+    assert Day4.letter_check(grid, %Point{x: 1, y: 1}, "X")
+  end
+
+  test "Given a Grid with the letter 'X' at %Point{x: 0, y: 0} then letter_check(Grid, %Point{x: 1, y: 1}, 'X') will return false" do
+    grid = %Grid{
+      width: 2,
+      height: 2,
+      map: %{%Point{x: 0, y: 0} => "X", %Point{x: 1, y: 1} => "M"}
+    }
+
+    refute Day4.letter_check(grid, %Point{x: 1, y: 1}, "X")
+  end
+
+  test "Given a Grid with width 2, when letter_check is provided a point with x = 2, then it will return false" do
+    grid = %Grid{
+      width: 2,
+      height: 2,
+      map: %{%Point{x: 0, y: 0} => "M", %Point{x: 2, y: 1} => "X"}
+    }
+
+    refute Day4.letter_check(grid, %Point{x: 2, y: 1}, "X")
+  end
+
+  test "Given a Grid with height 2, when letter_check is provided with a point with y = 2, then it will return false" do
+    grid = %Grid{
+      width: 2,
+      height: 2,
+      map: %{%Point{x: 0, y: 0} => "M", %Point{x: 1, y: 2} => "X"}
+    }
+
+    refute Day4.letter_check(grid, %Point{x: 1, y: 2}, "X")
+  end
+
+  test "Given a grid with XMAS written diagonally, a starting point of 0, 0, a direction of 1, 1, and text list ['X', 'M', 'A', 'S'] then scan will return true" do
+    grid = %Grid{
+      width: 4,
+      height: 4,
+      map: %{
+        %Point{x: 0, y: 0} => "X",
+        %Point{x: 1, y: 1} => "M",
+        %Point{x: 2, y: 2} => "A",
+        %Point{x: 3, y: 3} => "S"
+      }
+    }
+
+    assert Day4.scan(grid, %Point{x: 0, y: 0}, %Point{x: 1, y: 1}, ["X", "M", "A", "S"])
+  end
+
+  test "Given a grid with XMAS written up-left, a starting point of 0, 0, a direction of 1, 1 and text list ['X', 'M', 'A', 'S'] then the scan will return false" do
+    grid = %Grid{
+      width: 4,
+      height: 4,
+      map: %{
+        %Point{x: 3, y: 3} => "X",
+        %Point{x: 2, y: 2} => "M",
+        %Point{x: 1, y: 1} => "A",
+        %Point{x: 0, y: 0} => "S"
+      }
+    }
+
+    refute Day4.scan(grid, %Point{x: 0, y: 0}, %Point{x: 1, y: 1}, ["X", "M", "A", "S"])
+  end
+
+  test "Given a grid with XMAS written down-right, a starting point of 0, 0, a direction of -1, -1 and a text list ['S', 'A', 'M', 'X'] then scan will return true" do
+    grid = %Grid{
+      width: 4,
+      height: 4,
+      map: %{
+        %Point{x: 0, y: 0} => "X",
+        %Point{x: 1, y: 1} => "M",
+        %Point{x: 2, y: 2} => "A",
+        %Point{x: 3, y: 3} => "S"
+      }
+    }
+
+    assert Day4.scan(grid, %Point{x: 3, y: 3}, %Point{x: -1, y: -1}, ["S", "A", "M", "X"])
   end
 
   test "Given a grid of Point -> letter containing XMAS, word_count() will return 1" do
-    grid = %{
-      %Point{x: 0, y: 0} => "X",
-      %Point{x: 1, y: 0} => "M",
-      %Point{x: 2, y: 0} => "A",
-      %Point{x: 3, y: 0} => "S"
+    grid = %Grid{
+      width: 4,
+      height: 4,
+      map: %{
+        %Point{x: 0, y: 0} => "X",
+        %Point{x: 1, y: 0} => "M",
+        %Point{x: 2, y: 0} => "A",
+        %Point{x: 3, y: 0} => "S"
+      }
     }
 
-    assert Day4.word_count(grid, 4, 1) == 1
+    assert Day4.word_count(grid) == 1
   end
 
   test "Given a grid of Point -> letter containing SAMX, word_count() will return 1" do
-    grid = %{
-      %Point{x: 3, y: 0} => "X",
-      %Point{x: 2, y: 0} => "M",
-      %Point{x: 1, y: 0} => "A",
-      %Point{x: 0, y: 0} => "S"
+    grid = %Grid{
+      width: 4,
+      height: 4,
+      map: %{
+        %Point{x: 3, y: 0} => "X",
+        %Point{x: 2, y: 0} => "M",
+        %Point{x: 1, y: 0} => "A",
+        %Point{x: 0, y: 0} => "S"
+      }
     }
 
-    assert Day4.word_count(grid, 4, 1) == 1
+    assert Day4.word_count(grid) == 1
   end
 
   test "Given a grid of Point -> letter containing XMAS written down vertically, word_count()will return 1" do
-    grid = %{
-      %Point{x: 0, y: 0} => "X",
-      %Point{x: 0, y: 1} => "M",
-      %Point{x: 0, y: 2} => "A",
-      %Point{x: 0, y: 3} => "S"
+    grid = %Grid{
+      width: 4,
+      height: 4,
+      map: %{
+        %Point{x: 0, y: 0} => "X",
+        %Point{x: 0, y: 1} => "M",
+        %Point{x: 0, y: 2} => "A",
+        %Point{x: 0, y: 3} => "S"
+      }
     }
 
-    assert Day4.word_count(grid, 1, 4) == 1
+    assert Day4.word_count(grid) == 1
   end
 
   test "Given a grid of Point -> letter containing xmas written up vertically, word_count()will return 1" do
-    grid = %{
-      %Point{x: 0, y: 3} => "X",
-      %Point{x: 0, y: 2} => "M",
-      %Point{x: 0, y: 1} => "A",
-      %Point{x: 0, y: 0} => "S"
+    grid = %Grid{
+      width: 4,
+      height: 4,
+      map: %{
+        %Point{x: 0, y: 3} => "X",
+        %Point{x: 0, y: 2} => "M",
+        %Point{x: 0, y: 1} => "A",
+        %Point{x: 0, y: 0} => "S"
+      }
     }
 
-    assert Day4.word_count(grid, 1, 4) == 1
+    assert Day4.word_count(grid) == 1
   end
 
   test "Given a grid of Point -> letter containing xmas written diagonally down-left, word_count()will return 1" do
-    grid = %{
-      %Point{x: 3, y: 0} => "X",
-      %Point{x: 2, y: 1} => "M",
-      %Point{x: 1, y: 2} => "A",
-      %Point{x: 0, y: 3} => "S"
+    grid = %Grid{
+      width: 4,
+      height: 4,
+      map: %{
+        %Point{x: 3, y: 0} => "X",
+        %Point{x: 2, y: 1} => "M",
+        %Point{x: 1, y: 2} => "A",
+        %Point{x: 0, y: 3} => "S"
+      }
     }
 
-    assert Day4.word_count(grid, 4, 4) == 1
+    assert Day4.word_count(grid) == 1
   end
 
   test "Given a grid of point -> letter containing xmas written diagonally down-right, word_count()will return 1" do
-    grid = %{
-      %Point{x: 0, y: 0} => "X",
-      %Point{x: 1, y: 1} => "M",
-      %Point{x: 2, y: 2} => "A",
-      %Point{x: 3, y: 3} => "S"
+    grid = %Grid{
+      width: 4,
+      height: 4,
+      map: %{
+        %Point{x: 0, y: 0} => "X",
+        %Point{x: 1, y: 1} => "M",
+        %Point{x: 2, y: 2} => "A",
+        %Point{x: 3, y: 3} => "S"
+      }
     }
 
-    assert Day4.word_count(grid, 4, 4) == 1
+    assert Day4.word_count(grid) == 1
   end
 
   test "Given a grid of point -> letter containing xmas written diagonally up-right, word_count()will return 1" do
-    grid = %{
-      %Point{x: 0, y: 3} => "X",
-      %Point{x: 1, y: 2} => "M",
-      %Point{x: 2, y: 1} => "A",
-      %Point{x: 3, y: 0} => "S"
+    grid = %Grid{
+      width: 4,
+      height: 4,
+      map: %{
+        %Point{x: 0, y: 3} => "X",
+        %Point{x: 1, y: 2} => "M",
+        %Point{x: 2, y: 1} => "A",
+        %Point{x: 3, y: 0} => "S"
+      }
     }
 
-    assert Day4.word_count(grid, 4, 4) == 1
+    assert Day4.word_count(grid) == 1
   end
 
   test "Given a grid of point -> letter containing xmas written diagonally up-left, word_count()will return 1" do
-    grid = %{
-      %Point{x: 3, y: 3} => "X",
-      %Point{x: 2, y: 2} => "M",
-      %Point{x: 1, y: 1} => "A",
-      %Point{x: 0, y: 0} => "S"
+    grid = %Grid{
+      width: 4,
+      height: 4,
+      map: %{
+        %Point{x: 3, y: 3} => "X",
+        %Point{x: 2, y: 2} => "M",
+        %Point{x: 1, y: 1} => "A",
+        %Point{x: 0, y: 0} => "S"
+      }
     }
 
-    assert Day4.word_count(grid, 4, 4) == 1
+    assert Day4.word_count(grid) == 1
   end
 end
