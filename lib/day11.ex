f6bbf4726bfbb326d48f74465c69829205293f0c
Doug Bruce <queazan@gmail.com>
2024-12-11 18:56:13 -0600

    - mm.  One of the exponentials.  Probably not going to get a solution until I figure out the simplification.    Sadly, the answer isn't springing to mind.

--- lib/day11.ex
+++ lib/day11.ex
@@ -1,2 +1,71 @@
 defmodule Day11 do
+  require Logger
+
+  def run() do
+    lines =
+      Common.open(File.cwd(), "day11.txt")
+      |> Common.read_file_pipe()
+      |> Common.close()
+
+    stones = List.first(lines) |> String.split() |> Enum.map(&String.to_integer/1)
+
+    mutations =
+      for stone <- stones do
+        Logger.debug("Sequencing stone #{inspect(stone)}")
+        run_sequence(stone, 75)
+      end
+
+    # |> List.flatten()
+
+    # IO.puts("Mutations: #{inspect(mutations)}")
+    IO.puts("Total stone count: #{inspect(Enum.sum(mutations))}")
+  end
+
+  @spec run_sequence(integer(), integer()) :: integer()
+  def run_sequence(stone, 1) do
+    length(mutate(stone))
+    # 1
+  end
+
+  def run_sequence(stone, iterations_remaining) do
+    # Logger.debug("stone for iteration #{iterations_remaining}: #{inspect(stone)}")
+    products = mutate(stone)
+
+    temp =
+      Enum.map(products, &run_sequence(&1, iterations_remaining - 1))
+
+    # Logger.debug("#{inspect(temp)}")
+
+    temp2 = temp |> Enum.sum()
+
+    # Logger.debug("#{inspect(temp2)}")
+
+    temp2
+
+    # |> List.flatten()
+  end
+
+  @spec mutate(integer()) :: [integer()]
+  def mutate(0) do
+    [1]
+  end
+
+  def mutate(number) do
+    digit_count = Integer.to_string(number) |> String.length()
+
+    case rem(digit_count, 2) do
+      0 ->
+        shifter = Integer.pow(10, Integer.floor_div(digit_count, 2))
+        [Integer.floor_div(number, shifter), rem(number, shifter)]
+
+      1 ->
+        [number * 2024]
+    end
+
+    # [Integer.floor_div(even, shifter), rem(even, shifter)]
+  end
+
+  # def mutate(odd) do
+  #   Logger.debug("Odd path picked for stone #{inspect(odd)}")
+  # end
 end
--- test/day11_test.ex
+++ test/day11_test.ex
@@ -3,11 +3,26 @@ defmodule Day11Test do
   doctest Day11
 
   test "Given a stone marked 0, mutate will return a list of a single stone marked [1]" do
+    stone = 0
+
+    assert Day11.mutate(stone) == [1]
   end
 
   test "Given a stone marked with an even number of digits, mutate will return a pair of stones whose digits are the first half and the second half of the parent" do
+    stone = 123_456
+
+    assert Day11.mutate(stone) == [123, 456]
   end
 
   test "Given a stone marked with an odd number of digits, mutate will return a list of a single stone whose digits are the the parent's multiplied by 2024" do
+    stone = 12345
+
+    assert Day11.mutate(stone) == [12345 * 2024]
+  end
+
+  test "Given a single digit 0 and the number 4, run_sequence() will generate [2, 0, 2, 4]" do
+    stone = 0
+
+    assert Day11.run_sequence(stone, 4) == [2, 0, 2, 4]
   end
 end
